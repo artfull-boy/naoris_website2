f9a452d66b506ba14594fa17d90d9a866432d4a5
Ilias Rais <ivarrais2001@gmail.com>
2024-03-26 14:42:30 +0000

    Update HeroSection.js

--- src/Sections/HeroSection/HeroSection.js
+++ src/Sections/HeroSection/HeroSection.js
@@ -6,8 +6,10 @@ import earth from "../../assets/earth.png";
 const HeroSection = () => {
   return (
     <div className='overflow-x-hidden w-[100%] h-screen'>
-        <img src={earth} alt='eclipse' className='absolute w[100%] object-contain h-[100%] right-0'/>
-        <div className='container flex gap-0  h-[100%] justify-center items-center relative'>
+        <img src={eclipse} alt='eclipse' className='absolute max-w[100%] object-contain right-0'/>
+        <img src={mesh} alt='eclipse' className='absolute max-w[100%] object-contain right-0'/>
+        <img src={earth} alt='eclipse' className='absolute max-w[100%] object-contain right-0'/>
+        <div className='container flex gap-0  h-[100%] justify-start  items-center relative'>
             <p className='font-sans text-white lg:text-[60px] md:text-[50px] sm:text-[42px] text-[30px] font-bold text-center'>UNLEASHING THE FUTURE: </p>
             <p className='font-sans text-white lg:text-[60px] md:text-[50px] sm:text-[42px] text-[30px] font-bold text-center'>LEADING THE WEB 3.0</p>
             <p className='font-sans text-white lg:text-[60px] md:text-[50px] sm:text-[42px] text-[30px] font-bold text-center'>REVOLUTION</p>
