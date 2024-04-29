868773e6314027802bf926511441e22b6fdda80c
Ilias Rais <73229695+artfull-boy@users.noreply.github.com>
2024-04-29 06:35:01 +0000

    Update About.js

--- src/Sections/About/About.js
+++ src/Sections/About/About.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import { motion } from "framer-motion";
 
@@ -6,30 +5,32 @@ import "./about.css";
 import "../Testimonials/David.css";
 
 const About = () => {
-  const [isMobile, setIsMobile] = React.useState(typeof window !== 'undefined'&& window.innerWidth <= 508);
+  const [isMobile, setIsMobile] = React.useState(
+    typeof window !== "undefined" && window.innerWidth <= 508
+  );
 
-    React.useEffect(() => {
-        const handleResize = () => {
-            setIsMobile(window.innerWidth <= 508);
-        };
-
-        window.addEventListener("resize", handleResize);
-    }, []);
-    const quoteVariants = {
-      hidden: {
-        opacity: 0,
-        x: 500,
-      },
-      visible: {
-        opacity: 1,
-        x: 0,
-        transition: {
-          type: "spring",
-          stiffness: 100,
-          damping: 20,
-        },
-      },
+  React.useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 508);
     };
+
+    window.addEventListener("resize", handleResize);
+  }, []);
+  const quoteVariants = {
+    hidden: {
+      opacity: 0,
+      x: 500,
+    },
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        type: "spring",
+        stiffness: 100,
+        damping: 20,
+      },
+    },
+  };
   const testimonialsVariants = {
     hidden: {
       opacity: 0,
@@ -48,11 +49,14 @@ const About = () => {
   const spring = {
     type: "spring",
     damping: 10,
-    stiffness: 100
-  }
+    stiffness: 100,
+  };
   return (
     <div className="relative flex-col" suppressHydrationWarning={true}>
-      <div className="flex flex-col gap-20 relative z-[1]" suppressHydrationWarning={true}>
+      <div
+        className="flex flex-col gap-20 relative z-[1]"
+        suppressHydrationWarning={true}
+      >
         <div className="container">
           <motion.p
             initial={{
@@ -67,18 +71,16 @@ const About = () => {
             viewport={{ once: true }}
             className="font-semibold text-[30px] medium:text-[27px] vsm:text-[20px] text-white"
           >
-            Naoris is an American company founded under a mandate from the North Atlantic Treaty Organization (NATO)
-
-Hello All, this phrase on the website we need to change it to:
-
-Naoris is an American company founded under a mandate from previous leaders of the North Atlantic Treaty Organization (NATO), whose protocol represents a key
-            solution for sustainable, scalable, interoperable, and secure
-            digitalization suited to future challenges. With several European
-            cities as references, such as Dublin, Rijeka, Zurich, and Vienna
-            under the Smart Cities Initiative, and the telecommunications giant
-            T-Mobile, the company merges blockchain and artificial intelligence
-            (AI) to revolutionize the digital world while offering advanced
-            protection, including against post-quantum threats.
+            Naoris is an American company founded under a mandate from previous
+            leaders of the North Atlantic Treaty Organization (NATO), whose
+            protocol represents a key solution for sustainable, scalable,
+            interoperable, and secure digitalization suited to future
+            challenges. With several European cities as references, such as
+            Dublin, Rijeka, Zurich, and Vienna under the Smart Cities
+            Initiative, and the telecommunications giant T-Mobile, the company
+            merges blockchain and artificial intelligence (AI) to revolutionize
+            the digital world while offering advanced protection, including
+            against post-quantum threats.
           </motion.p>
         </div>
         {isMobile ? (
@@ -131,23 +133,24 @@ Naoris is an American company founded under a mandate from previous leaders of t
           </motion.div>
         )}
       </div>
-      <motion.img 
-      src={`${process.env.PUBLIC_URL}/assets/about_element.png`}
-      alt="World Shape"
-      initial={{
-        opacity:0,
-        scale:0,
-        rotate:180
-      }}
-      whileInView={{
-        opacity:1,
-        scale:1,
-        rotate:180
-      }}
-      transition={spring}
-      width={600}
-      viewport={{ once: true }}
-      className="absolute rotate-180 left-[-10%] lg:bottom-[-40%] vsm:w-[400px] vsm:hidden md:bottom-3"></motion.img>
+      <motion.img
+        src={`${process.env.PUBLIC_URL}/assets/about_element.png`}
+        alt="World Shape"
+        initial={{
+          opacity: 0,
+          scale: 0,
+          rotate: 180,
+        }}
+        whileInView={{
+          opacity: 1,
+          scale: 1,
+          rotate: 180,
+        }}
+        transition={spring}
+        width={600}
+        viewport={{ once: true }}
+        className="absolute rotate-180 left-[-10%] lg:bottom-[-40%] vsm:w-[400px] vsm:hidden md:bottom-3"
+      ></motion.img>
     </div>
   );
 };
